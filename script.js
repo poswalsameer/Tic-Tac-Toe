0013e44503b1f3392add2a31a3a84c7097988772
poswalsameer <poswalsameer2003@gmail.com>
2023-11-20 23:50:42 +0530

    Made some changes on the main menu screen and added a svg on the game screen for back button

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 console.log("The javascript is working fine");
 
+// function that takes to the main game screen from main menu on button click
 const playGame = () => {
-
     window.location.href = "game.html";
+}
 
+// function that takes us back to the main menu on button click
+const backToMenu = () => {
+    window.location.href = "index.html";
+}
+
+//back button on the game screen to go back to the main menu
+const goBack = () => {
+    window.location.href = "index.html";
 }

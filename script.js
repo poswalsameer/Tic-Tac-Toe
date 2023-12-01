7daf3a38bc6ab4977c97a0e33f2fe018f18352a4
poswalsameer <poswalsameer2003@gmail.com>
2023-12-01 21:02:53 +0530

    Made the game more beautiful and added css, next thing is to work on the winning logic

--- script.js
+++ script.js
@@ -35,13 +35,39 @@ const changeTurn = () => {
     }
 }
 
+//Function that changes the text in UI after every turn 
+const changeTurnText = () => {
+    document.getElementsByClassName('turnText')[0].innerText = turn;
+
+    if( turn === 'X' ){
+        document.getElementsByClassName('turnText')[0].style.color = "rgb(86, 60, 10)";
+    }
+    else{
+        document.getElementsByClassName('turnText')[0].style.color = "green";
+    }
+}
+
+
+
 //Logic to add text in the tic tac toe table
 Array.from(box).forEach(element => {
     element.addEventListener('click', ()=> {
         element.innerText = turn;
+        if( turn === 'X' ){
+            element.style.color = "rgb(137, 105, 47)";
+        }
+        else{
+            element.style.color = "green";
+        }
         changeTurn();
+        console.log("change turn is working");
+        changeTurnText();
+        // if()
     })
 })
+
+
+
 
 
 
